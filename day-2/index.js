4e78b10d2b627c8d525656a4903da7b61c9446df
sulaymon333 <tjsulaymon@gmail.com>
2025-10-06 20:43:16 +0300

    complete day 2 level 1 exercise

--- day-2/index.js
+++ day-2/index.js
@@ -1,6 +1,44 @@
 // Exercise level 1
+const challenge = '30 Days Of JavaScript';
+// let challenge = '30 Days Of JavaScript';
 
-// 1
+// 2
+console.log(challenge);
+
+// 3
+console.log(challenge.length);
+
+// 4
+
+const challenge2 = challenge.toUpperCase();
+console.log(challenge.toUpperCase());
+// challenge = challenge.toUpperCase();
+
+console.log(challenge2);
+
+// 5
+const challenge3 = challenge.toLowerCase();
+console.log(challenge3);
+
+// 6
+const challenge4 = challenge.substring(0, 2);
+challenge4;
+
+// 7
+const challenge5 = challenge.substring(3);
+challenge5;
+
+// 8
+const ch1 = challenge.toLowerCase().includes('script');
+ch1;
+
+// 9
+const ch2 = challenge.split();
+ch2;
+
+// 10
+const ch3 = challenge.split(' ');
+ch2;
 
 // Exercise level 2
 // 1
